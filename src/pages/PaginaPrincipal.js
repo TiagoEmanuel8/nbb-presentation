d85008e85cdab951e02697c65330f67e5d9b8179
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-12-25 10:21:14 -0300

    configurando navegaçao entre as paginas

--- src/pages/PaginaPrincipal.js
+++ src/pages/PaginaPrincipal.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class PaginaPrincipal extends React.Component {
   render() {
     return(
-      <p>página principal</p>
+      <section>
+        <p>página principal</p>
+        <Link to={'/team'}>
+        <button>Conheça as equipes</button>
+        </Link>
+      </section>
     )
   }
 };
