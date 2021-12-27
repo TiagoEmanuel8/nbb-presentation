30bd00351629417f41f72873d07f89b887d0cf1a
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-12-27 10:22:43 -0300

    teste estilizaçao para pagina de detalhes

--- src/pages/PaginaDetalhe.js
+++ src/pages/PaginaDetalhe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/paginaDetalhe.css'
 
 class PaginaDetalhe extends React.Component {
   render() {
@@ -6,17 +7,18 @@ class PaginaDetalhe extends React.Component {
     const { databaseTeams } = this.props;
     const timeAtual = databaseTeams[id - 1];
     return(
-      <section>
-         <p>{ `Nome: ${timeAtual.nome}` }</p>
-         <p>{ `Nome Completo: ${timeAtual.nomeCompleto}` }</p>
-         <p>{ `Ano de fundação: ${timeAtual.fundacao}` }</p>
-         <p>{ `Mascote: ${timeAtual.mascote}` }</p>
-         <p>{ `Localização: ${timeAtual.localizacao}` }</p>
-         <p>{ `Arena: ${timeAtual.arena}` }</p>
-         <p>{ `Capacidade da Arena: ${timeAtual.capacidadeArena}` }</p>
-         <p>{ `Material Esportivo: ${timeAtual.materialEsportivo}` }</p>
+      <section className='detalhes-time'>
          <img alt="imagem de times" src={`../${timeAtual.imagem}`} />
-
+         <div className='texto-time'>
+          <p className={`time-${timeAtual.nome}`}>{ `Nome: ${timeAtual.nome}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Nome Completo: ${timeAtual.nomeCompleto}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Ano de fundação: ${timeAtual.fundacao}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Mascote: ${timeAtual.mascote}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Localização: ${timeAtual.localizacao}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Arena: ${timeAtual.arena}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Capacidade da Arena: ${timeAtual.capacidadeArena}` }</p>
+          <p className={`time-${timeAtual.nome}`}>{ `Material Esportivo: ${timeAtual.materialEsportivo}` }</p>
+         </div>
       </section>
     )
   }
